ad291b0383afcb0d58b0f139e8faeab55d5f8c87
Danylo Melnychuk <danyakid2005@gmail.com>
2025-03-25 19:15:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command!")
+        return
+
+    src, dest = parts[1], parts[2]
+
+    if src == dest:
+        print("Source file is equal to Destination!")
+        return
+
+    try:
+        with open(src, "r") as f_src, open(dest, "w") as f_dest:
+            f_dest.write(f_src.read())
+    except Exception as e:
+        print(f"Error with copying: {e}")
